5b44998adb7a986acf86f605a39ca3341f6bea7b
Eric Nguyen <tuk94307@temple.edu>
2022-07-26 23:53:14 -0400

    Refactor album_page.dart

--- lib/ui/album_page.dart
+++ lib/ui/album_page.dart
@@ -17,87 +17,176 @@ class AlbumPage extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          MainAppBar(
-            title: Text(
-              album.title,
-              style: const TextStyle(
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            background: Image.asset(
-              album.imagePath,
-              fit: BoxFit.cover,
-            ),
-            expandedHeight: 256,
+          _AlbumPageAppBar(
+            album: album,
           ),
           const SliverToBoxAdapter(
-            child: Padding(padding: EdgeInsets.all(2.0)),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => FutureBuilder(
-                future: DefaultAssetBundle.of(context).loadString(
-                    '${album.lyricsFolderPath}/${album.lyricsPaths[index]}'),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final Song song =
-                        Song.fromJson(jsonDecode(snapshot.data.toString()));
-                    return Card(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                        vertical: 4.0,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        side: const BorderSide(color: Colors.white38, width: 1),
-                      ),
-                      clipBehavior: Clip.hardEdge,
-                      child: InkWell(
-                        onTap: () {
-                          if (song.lyrics.isNotEmpty) {
-                            Navigator.of(context).push(
-                              PageTransition.to(
-                                LyricsPage(
-                                  imagePath: album.imagePath,
-                                  song: song,
-                                ),
-                              ),
-                            );
-                          }
-                        },
-                        child: ListTile(
-                          leading: Text('${index + 1}'),
-                          title: Text(
-                            song.title,
-                            style: TextStyle(
-                              fontFamily: 'Raleway',
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: song.lyrics.isEmpty
-                                  ? Theme.of(context).disabledColor
-                                  : Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      ?.color,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  } else {
-                    return const ListTile(
-                      title: Text('Error'),
-                    );
-                  }
-                },
-              ),
-              childCount: album.lyricsPaths.length,
+            child: Padding(
+              padding: EdgeInsets.all(2.0),
             ),
+          ),
+          _AlbumPageList(
+            album: album,
           ),
         ],
       ),
+    );
+  }
+}
+
+class _AlbumPageAppBar extends StatelessWidget {
+  final Album album;
+
+  const _AlbumPageAppBar({
+    Key? key,
+    required this.album,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainAppBar(
+      title: Text(
+        album.title,
+        style: const TextStyle(
+          fontFamily: 'Raleway',
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
+      background: Image.asset(
+        album.imagePath,
+        fit: BoxFit.cover,
+      ),
+      expandedHeight: 256,
+    );
+  }
+}
+
+class _AlbumPageList extends StatelessWidget {
+  final Album album;
+
+  const _AlbumPageList({
+    Key? key,
+    required this.album,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => _AlbumPageListItem(
+          album: album,
+          index: index,
+        ),
+        childCount: album.lyricsPaths.length,
+      ),
+    );
+  }
+}
+
+class _AlbumPageListItem extends StatelessWidget {
+  final Album album;
+  final int index;
+
+  const _AlbumPageListItem({
+    Key? key,
+    required this.album,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString(_songPath(index)),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final Song song = Song.fromJson(jsonDecode(snapshot.data.toString()));
+
+          return Card(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 4.0,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              side: const BorderSide(color: Colors.white38, width: 1),
+            ),
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: () {
+                if (song.lyrics.isNotEmpty) {
+                  _goToLyricsPage(context, album.imagePath, song);
+                }
+              },
+              child: _AlbumPageListItemTitle(
+                index: index,
+                song: song,
+              ),
+            ),
+          );
+        } else {
+          return _AlbumPageListItemError();
+        }
+      },
+    );
+  }
+
+  String _songPath(int index) {
+    return '${album.lyricsFolderPath}/${album.lyricsPaths[index]}';
+  }
+
+  void _goToLyricsPage(
+    BuildContext context,
+    String imagePath,
+    Song song,
+  ) {
+    Navigator.of(context).push(
+      PageTransition.to(
+        LyricsPage(
+          imagePath: imagePath,
+          song: song,
+        ),
+      ),
+    );
+  }
+}
+
+class _AlbumPageListItemTitle extends StatelessWidget {
+  final Song song;
+  final int index;
+
+  const _AlbumPageListItemTitle({
+    Key? key,
+    required this.song,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text('${index + 1}'),
+      title: Text(
+        song.title,
+        style: TextStyle(
+          fontFamily: 'Raleway',
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: song.lyrics.isEmpty
+              ? Theme.of(context).disabledColor
+              : Theme.of(context).textTheme.bodyText1?.color,
+        ),
+      ),
+    );
+  }
+}
+
+class _AlbumPageListItemError extends StatelessWidget {
+  static const String errorMessage = 'Error';
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTile(
+      title: Text(errorMessage),
     );
   }
 }
